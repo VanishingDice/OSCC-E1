c85a18ed5d2163349078ce8d76ec6ce275b5c0f2
vanishingdice <apple9seeds@163.com>
2026-09-04 19:37:38 +0800

    finished

--- ch11s04.c
+++ ch11s04.c
@@ -5,33 +5,41 @@ int a[LEN] = { 5, 2, 4, 7, 1, 3, 2, 6 };
 
 int partition(int start, int end)
 {
-	// 从a[start..end]中选取一个pivot元素（比如选a[start]为pivot）;
+	// 从a[start..end]中选取一ot元素（比如选a[start]为pivot）;
 	// 在一个循环中移动a[start..end]的数据，将a[start..end]分成两半，
 	// 使a[start..mid-1]比pivot元素小，a[mid+1..end]比pivot元素大，而a[mid]就是pivot元素;
 	// return mid;
     int pivot = a[start];
     
     while (start < end) {
-        while (a[end] >= pivot && end > start) {
+        while (a[end] >= pivot) {
             --end;
+            if (start == end) {
+                a[start] = pivot;
+                return start;
+            }
         }
         a[start] = a[end];
         ++start;
+        if (start == end){
+            a[start] = pivot;
+            return start;
+        }
         while (a[start] <= pivot) {
             ++start;
+            if (start == end) {
+                a[start] = pivot;
+                return start;
+            }
         }
         a[end] = a[start];
         --end;
+        if (start == end) {
+            a[start] = pivot;
+            return start;
+        }
     }
-    a[start] = pivot;
-    return start;
-}
-
-void exchange(index1, index2) {
-    int tmp;
-    tmp = a[index1];
-    a[index1] = a[index2];
-    a[index2] = tmp;
+    return -1;
 }
 
 void quicksort(int start, int end)
