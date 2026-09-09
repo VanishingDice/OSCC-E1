be8b9df59c1853b4364ddb0918e0cee41381f89a
vanishingdice <apple9seeds@163.com>
2026-09-09 19:11:24 +0800

    array weird test update

--- test.c
+++ test.c
@@ -5,4 +5,8 @@ int main(void) {
     a[0] = 123;
     a[1] = 456;
     printf("%d\n", 1[a]);
+    int *pa = a;
+    pa++;
+    printf("negative index:%d\n", pa[-1]);
+    printf("%u\n", pa);//print the address stored in pa
 }
