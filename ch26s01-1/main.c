b2b3ba3d6ec3ad3c4945a0487c588332ef9054fe
vanishingdice <apple9seeds@163.com>
2026-09-14 21:25:49 +0800

    ch26s01-1 finished

---
+++ ch26s01-1/main.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include "linkedlist.h"
+
+void print_item(link p) {
+    printf("%d\n", p->item);
+}
+
+void sort_test(void) {
+	link p = make_node(10);
+	order_insert(p);
+	p = make_node(5);
+	order_insert(p);
+	p = make_node(20);
+	order_insert(p);
+	p = make_node(21);
+	order_insert(p);
+	p = make_node(6);
+	order_insert(p);
+	p = make_node(5);
+	order_insert(p);
+	p = make_node(20);
+	order_insert(p);
+	p = make_node(10);
+	order_insert(p);
+	p = make_node(5);
+	order_insert(p);
+	p = make_node(6);
+	order_insert(p);
+	p = make_node(7);
+	order_insert(p);
+	p = make_node(0);
+	order_insert(p);
+	traverse(print_item);
+	destroy();
+}
+
+void test(void) {
+ 	link p = make_node(10);
+	insert(p);
+	p = make_node(5);
+	insert(p);
+	p = make_node(90);
+	insert(p);
+	p = search(5);
+	delete(p);
+	free_node(p);
+	traverse(print_item);
+	destroy();
+
+	p = make_node(100);
+	push(p);
+	p = make_node(200);
+	push(p);
+	p = make_node(250);
+	push(p);
+	while (p = pop()) {
+		print_item(p);
+		free_node(p);
+	}
+
+}
+
+void queue_test() {
+	link p = make_node(10);
+	enqueue(p);
+	p = make_node(5);
+	enqueue(p);
+	p = make_node(20);
+	enqueue(p);
+	p = make_node(21);
+	enqueue(p);
+	p = make_node(6);
+	enqueue(p);
+	p = make_node(5);
+	enqueue(p);
+	p = make_node(20);
+	enqueue(p);
+	p = make_node(10);
+	enqueue(p);
+	p = make_node(5);
+	enqueue(p);
+	p = make_node(6);
+	enqueue(p);
+	p = make_node(7);
+	enqueue(p);
+	p = make_node(0);
+	enqueue(p);
+	dequeue();
+	dequeue();
+	dequeue();
+	reverse();
+	traverse(print_item);
+	destroy();
+}
+
+int main(void) {
+   
+	// test();
+	// sort_test();
+	queue_test();
+
+	return 0;
+}
